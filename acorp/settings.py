407006703c77428ad9f3f9342b01f6fd284dcd7a
Eremeev Alexandr <37283745+eremeevalexandr@users.noreply.github.com>
2023-05-12 14:58:13 +0500

    изменения стилей

--- acorp/settings.py
+++ acorp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-i45uf@k#xst9(e^e9*r64a#dd95807496eo%)j_xi1*xot=^zk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.1.1.240']
 
 # Application definition
 
@@ -117,6 +117,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
