04c1af131ff5f37bc81044dddf8c4034d61c493d
Eremeev Alexandr <37283745+eremeevalexandr@users.noreply.github.com>
2023-05-17 14:20:29 +0500

    вывод ошибок регистрации и входа

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib import messages
 
 
 def register(request):
@@ -15,9 +16,11 @@ def register(request):
             user = authenticate(username=user.username, password=password)
             login(request, user)
             return redirect('helpdesk:index')
+        else:
+            messages.error(request, form.errors)
     else:
         form = CustomUserCreationForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {'form': form},)
 
 
 def login_view(request):
@@ -28,9 +31,11 @@ def login_view(request):
             password = form.cleaned_data.get('password')
             user = authenticate(username=email, password=password)
             if user is not None:
-                login(request, user)
+                # Пользователь успешно аутентифицирован
                 return redirect('helpdesk:index')
+            else:
+                # Пользователь не аутентифицирован, добавляем сообщение об ошибке
+                messages.error(request, "Неправильный email или пароль")
     else:
         form = CustomAuthenticationForm()
-    print(form.errors)
     return render(request, 'accounts/login.html', {'form': form})
