95529773bf6ec3ccebbe111a93bfd36f4f1cecb9
Eremeev Alexandr <37283745+eremeevalexandr@users.noreply.github.com>
2023-05-17 14:21:02 +0500

    1

--- acorp/settings.py
+++ acorp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-i45uf@k#xst9(e^e9*r64a#dd95807496eo%)j_xi1*xot=^zk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.1.1.240']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.1.1.242', '172.30.30.242', 'kon-service.local']
 
 # Application definition
 
@@ -113,11 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+ROOT_PATH = os.path.dirname(__file__)
+
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
