9da70665edc496cbec1e2eb126eafec1b1a84b15
Eremeev Alexandr <37283745+eremeevalexandr@users.noreply.github.com>
2023-05-18 12:10:12 +0500

    вывод в админке django удобной информации

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Ticket
 
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ['user', 'title', 'created_at', 'status', 'deadline', 'updated_at']
-    list_filter = ['user']
+    list_filter = ['status', 'user', 'created_at', 'updated_at', 'deadline']
+
