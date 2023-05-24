230c2d352c92f6b2b286db8879daf3c7b1c09440
Eremeev Alexandr <37283745+eremeevalexandr@users.noreply.github.com>
2023-05-24 18:21:50 +0500

    поля для изменения статуса пользователем

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create_ticket, name='create_ticket'),
     path('<int:ticket_id>/', views.view_ticket, name='view_ticket'),
+    path('ticket/<int:ticket_id>/complete/', views.complete_ticket, name='complete_ticket'),
+    path('ticket/<int:ticket_id>/reopen/', views.reopen_ticket, name='reopen_ticket'),
 ]
