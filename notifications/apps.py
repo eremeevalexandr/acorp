936098aa36d198d968af20cce58636e81351c04a
Eremeev Alexandr <37283745+eremeevalexandr@users.noreply.github.com>
2023-05-24 18:14:50 +0500

    добавлен комментарий

--- notifications/apps.py
+++ notifications/apps.py
@@ -5,5 +5,6 @@ class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
 
+    # импортируем сигналы с отправкой писем
     def ready(self):
         import notifications.signals
